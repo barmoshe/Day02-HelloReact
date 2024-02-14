81fe04c0a1fd4544a1155b817d06342b1843eb73
barmoshe <1barmoshe1@gmail.com>
2024-02-14 14:19:19 +0200

    initial starter

---
+++ react-starter/RootCmp.jsx
@@ -0,0 +1,16 @@
+
+import { AppHeader } from "./cmps/AppHeader.jsx"
+import { Home } from "./cmps/Home.jsx"
+
+export function RootCmp() {
+    return (
+        <section className="app main-layout">
+            <AppHeader />
+            <main>
+                <main>
+                    <Home />
+                </main>
+            </main>
+        </section>
+    )
+}
